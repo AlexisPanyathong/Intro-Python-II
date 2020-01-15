e137d7c00a5e7ad32e48a33c27d2169b032037bd
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-01-15 15:41:57 -0600

    Progress:
    I added Player and Room classes and now working in the adv.py file.

--- src/player.py
+++ src/player.py
@@ -1,2 +1,6 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, cur_name, cur_room):
+        self.cur_name = cur_name
+        self.cur_room = cur_room
